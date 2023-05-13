003429d2657447b3caddbbf0f219feee22758f86
Klassik <81759469+Klassik2506@users.noreply.github.com>
2023-05-14 00:01:54 +0400

    Update conftest.py
    Правки

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def init_browser():
     browser.config.window_width = 720
     browser.config.window_height = 1080
